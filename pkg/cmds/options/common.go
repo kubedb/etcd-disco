7b090f20576e324ca9fda1d41f5a9a3a5e346e89
sanjid133 <sanjid@appscode.com>
2018-05-21 17:28:44 +0600

    find self id and revendor

--- pkg/cmds/options/common.go
+++ pkg/cmds/options/common.go
@@ -7,6 +7,9 @@ import (
 	"github.com/spf13/cobra"
 	"github.com/spf13/pflag"
 	//	"github.com/pkg/errors"
+	"fmt"
+
+	"github.com/appscode/go/net"
 	"github.com/etcd-manager/lector/pkg/etcd"
 )
 
@@ -46,7 +49,7 @@ func (cfg *EtcdClusterConfig) AddFlags(fs *pflag.FlagSet) {
 
 	//fs.StringVar(&cfg.ClusterType, "cluster-type", cfg.ClusterType, "cluster type (seed/join)")
 	fs.StringArrayVar(&cfg.ServerAddress, "server-address", cfg.ServerAddress, "List of URLs to listen on for peer traffic. (required for join)")
-	fs.StringVar(&cfg.SelfAddrss, "self-address", cfg.SelfAddrss, "Own address")
+	//fs.StringVar(&cfg.SelfAddrss, "self-address", cfg.SelfAddrss, "Own address")
 	//	fs.IntVar(&cfg.ClusterSize, "cluster-size", cfg.ClusterSize, "Size of actual cluster(number of master)")
 	//fs.StringVar(&cfg.DiscoveryFile, "discovery-file", cfg.DiscoveryFile, "Disovery file location")
 	//fs.StringVar(&cfg.SnapshotDir, "snapshot-dir", cfg.SnapshotDir, "Snapshot directory location")
@@ -55,6 +58,15 @@ func (cfg *EtcdClusterConfig) AddFlags(fs *pflag.FlagSet) {
 func (cfg *EtcdClusterConfig) ValidateFlags(cmd *cobra.Command, args []string) error {
 	ensureFlags := []string{"name"}
 	flags.EnsureRequiredFlags(cmd, ensureFlags...)
+	ips, _, err := net.RoutableIPs()
+	if err != nil {
+		return fmt.Errorf("failed to detect routable ips. Reason: %v", err)
+	}
+	if len(ips) == 0 {
+		return fmt.Errorf("no routable ips found")
+	}
+	cfg.SelfAddrss = ips[0]
+	fmt.Println("found self address = ", cfg.SelfAddrss, "**************")
 	return nil
 
 }
