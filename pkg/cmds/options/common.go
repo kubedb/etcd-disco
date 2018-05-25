1a513e499c23c21154b17b2cfbf52d1c4136454e
sanjid133 <sanjid@appscode.com>
2018-05-25 10:12:39 +0600

    Fix cluster (#2)
    * etcd-cloud-operator as etcd
    
    * find self id and revendor
    
    * complete

--- pkg/cmds/options/common.go
+++ pkg/cmds/options/common.go
@@ -7,6 +7,9 @@ import (
 	"github.com/spf13/cobra"
 	"github.com/spf13/pflag"
 	//	"github.com/pkg/errors"
+	"fmt"
+
+	"github.com/appscode/go/net"
 	"github.com/etcd-manager/lector/pkg/etcd"
 )
 
@@ -17,21 +20,25 @@ const (
 
 type EtcdClusterConfig struct {
 	*etcd.ServerConfig
-	ClusterType   string
-	ServerAddress string
+	//ClusterType   string
+	ServerAddress []string
+	SelfAddrss    string
+	//ClusterSize   int
 }
 
 func NewEtcdClusterConfig() *EtcdClusterConfig {
 	return &EtcdClusterConfig{
 		&etcd.ServerConfig{
-			CheckInterval:        15 * time.Second,
-			UnhealthyMemberTTL:   30 * time.Second,
+			CheckInterval:        30 * time.Minute,
+			UnhealthyMemberTTL:   2 * time.Minute,
 			AutoDisasterRecovery: true,
 
-			SnapshotInterval: 2 * time.Minute,
+			SnapshotInterval: 24 * time.Hour,
 		},
-		ClusterTypeSeed,
+		//ClusterTypeSeed,
+		[]string{},
 		"",
+		//	1,
 	}
 }
 
@@ -40,8 +47,10 @@ func (cfg *EtcdClusterConfig) AddFlags(fs *pflag.FlagSet) {
 	fs.DurationVar(&cfg.UnhealthyMemberTTL, "unhealthy-member-ttl", cfg.UnhealthyMemberTTL, "The time after which, an unhealthy member will be removed from the cluster.")
 	fs.BoolVar(&cfg.AutoDisasterRecovery, "auto-disaster-recovery", cfg.AutoDisasterRecovery, "Defines whether the operator will attempt to seed a new cluster from a snapshot after the managed cluster has lost quorum")
 
-	fs.StringVar(&cfg.ClusterType, "cluster-type", cfg.ClusterType, "cluster type (seed/join)")
-	fs.StringVar(&cfg.ServerAddress, "server-address", "", "List of URLs to listen on for peer traffic. (required for join)")
+	//fs.StringVar(&cfg.ClusterType, "cluster-type", cfg.ClusterType, "cluster type (seed/join)")
+	fs.StringArrayVar(&cfg.ServerAddress, "server-address", cfg.ServerAddress, "List of URLs to listen on for peer traffic. (required for join)")
+	//fs.StringVar(&cfg.SelfAddrss, "self-address", cfg.SelfAddrss, "Own address")
+	//	fs.IntVar(&cfg.ClusterSize, "cluster-size", cfg.ClusterSize, "Size of actual cluster(number of master)")
 	//fs.StringVar(&cfg.DiscoveryFile, "discovery-file", cfg.DiscoveryFile, "Disovery file location")
 	//fs.StringVar(&cfg.SnapshotDir, "snapshot-dir", cfg.SnapshotDir, "Snapshot directory location")
 }
@@ -49,6 +58,15 @@ func (cfg *EtcdClusterConfig) AddFlags(fs *pflag.FlagSet) {
 func (cfg *EtcdClusterConfig) ValidateFlags(cmd *cobra.Command, args []string) error {
 	ensureFlags := []string{"name"}
 	flags.EnsureRequiredFlags(cmd, ensureFlags...)
+	ips, _, err := net.RoutableIPs()
+	if err != nil {
+		return fmt.Errorf("failed to detect routable ips. Reason: %v", err)
+	}
+	if len(ips) == 0 {
+		return fmt.Errorf("no routable ips found")
+	}
+	cfg.SelfAddrss = ips[0]
+	fmt.Println("found self address = ", cfg.SelfAddrss, "**************")
 	return nil
 
 }
---
+++ pkg/operator/misc.go
@@ -0,0 +1,167 @@
+// Copyright 2017 Quentin Machu & eco authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package operator
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"sort"
+	"sync"
+	"time"
+
+	"github.com/etcd-manager/lector/pkg/etcd"
+	"github.com/etcd-manager/lector/pkg/providers/snapshot"
+	log "github.com/sirupsen/logrus"
+)
+
+const (
+	isHealthRetries  = 3
+	isHealthyTimeout = 10 * time.Second
+)
+
+type status struct {
+	instance string
+
+	State    string `json:"state"`
+	Revision int64  `json:"revision"`
+}
+
+func initSnapshotProvider(cfg snapshot.Config) snapshot.Provider {
+	snapshotProvider, ok := snapshot.AsMap()[cfg.Provider]
+	if !ok {
+		log.Fatalf("unknown snapshot provider %q, available providers: %v", cfg.Provider, snapshot.AsList())
+	}
+	if err := snapshotProvider.Configure(cfg); err != nil {
+		log.WithError(err).Fatal("failed to configure snapshot provider")
+	}
+
+	return snapshotProvider
+}
+
+/*func initProviders(cfg Config) (asg.Provider, snapshot.Provider) {
+	if cfg.ASG.Provider == "" {
+		log.Fatal("no auto-scaling group provider configuration given")
+	}
+	asgProvider, ok := asg.AsMap()[cfg.ASG.Provider]
+	if !ok {
+		log.Fatalf("unknown auto-scaling group provider %q, available providers: %v", cfg.ASG.Provider, asg.AsList())
+	}
+	if err := asgProvider.Configure(cfg.ASG); err != nil {
+		log.WithError(err).Fatal("failed to configure auto-scaling group provider")
+	}
+
+	if cfg.Snapshot.Provider == "" {
+		return asgProvider, nil
+	}
+	snapshotProvider, ok := snapshot.AsMap()[cfg.Snapshot.Provider]
+	if !ok {
+		log.Fatalf("unknown snapshot provider %q, available providers: %v", cfg.Snapshot.Provider, snapshot.AsList())
+	}
+	if err := snapshotProvider.Configure(cfg.Snapshot); err != nil {
+		log.WithError(err).Fatal("failed to configure snapshot provider")
+	}
+
+	return asgProvider, snapshotProvider
+}*/
+
+func fetchStatuses(httpClient *http.Client, etcdClient *etcd.Client, Instances []string, self string) (bool, bool, map[string]int) {
+	var wg sync.WaitGroup
+	var mu sync.Mutex
+	wg.Add(1 + len(Instances))
+
+	// Fetch etcd's healthiness.
+	var etcdHealthy bool
+	go func() {
+		defer wg.Done()
+		etcdHealthy = etcdClient != nil && etcdClient.IsHealthy(isHealthRetries, isHealthyTimeout)
+	}()
+
+	// Fetch ECO statuses.
+	var ecoStatuses []*status
+	for _, instance := range Instances {
+		go func(instance string) {
+			defer wg.Done()
+
+			st, err := fetchStatus(httpClient, instance)
+			if err != nil {
+				log.WithError(err).Warnf("failed to query %s's instance", instance)
+				return
+			}
+
+			mu.Lock()
+			defer mu.Unlock()
+
+			ecoStatuses = append(ecoStatuses, st)
+		}(instance)
+	}
+	wg.Wait()
+
+	// Sort the ECO statuses so we can systematically find the identity of the seeder.
+	sort.Slice(ecoStatuses, func(i, j int) bool {
+		if ecoStatuses[i].Revision == ecoStatuses[j].Revision {
+			return ecoStatuses[i].instance < ecoStatuses[j].instance
+		}
+		return ecoStatuses[i].Revision < ecoStatuses[j].Revision
+	})
+
+	// Count ECO statuses and determine if we are the seeder.
+	ecoStates := make(map[string]int)
+	for _, ecoStatus := range ecoStatuses {
+		if _, ok := ecoStates[ecoStatus.State]; !ok {
+			ecoStates[ecoStatus.State] = 0
+		}
+		ecoStates[ecoStatus.State]++
+	}
+	fmt.Println(ecoStatuses[len(ecoStatuses)-1].instance, self, "**********")
+
+	return etcdHealthy, ecoStatuses[len(ecoStatuses)-1].instance == self, ecoStates
+}
+
+func fetchStatus(httpClient *http.Client, instance string) (*status, error) {
+	resp, err := httpClient.Get(fmt.Sprintf("http://%s:%d/status", instance, webServerPort))
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var st status
+	err = json.Unmarshal(b, &st)
+	st.instance = instance
+	fmt.Println(st, "<<>>>>>>>>>>>")
+	return &st, err
+}
+
+func serverConfig(cfg Config, snapshotProvider snapshot.Provider) *etcd.ServerConfig {
+	return &etcd.ServerConfig{
+		UnhealthyMemberTTL: cfg.UnhealthyMemberTTL,
+		SnapshotProvider:   snapshotProvider,
+		SnapshotInterval:   cfg.Snapshot.Interval,
+		SnapshotTTL:        cfg.Snapshot.TTL,
+	}
+}
+
+func stringOverride(s, override string) string {
+	if override != "" {
+		return override
+	}
+	return s
+}
